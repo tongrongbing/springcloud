729c732167e80128147ca332a0f4cf3067e5e4b1
tongrongbing <1099857577@qq.com>
2020-05-17 17:06:07 +0800

    first commit

---
+++ eureka-server/src/main/java/com/home/springclound/EurekaServerApplication.java
@@ -0,0 +1,18 @@
+package com.home.springclound;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author：tongrongbing
+ * @date：created in 2020/5/14 23:40
+ * @description：
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class,args);
+    }
+}
